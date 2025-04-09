a954341cd1109ecf96ea115e3dd0a7fefe48f02a
Gellert Toth <stgellertg@gmail.com>
2025-04-09 15:56:44 +0200

    implement homework_1

--- homework_1/project1.js
+++ homework_1/project1.js
@@ -4,4 +4,18 @@
 // fgPos is the position of the foreground image in pixels. It can be negative and (0,0) means the top-left pixels of the foreground and background are aligned.
 function composite( bgImg, fgImg, fgOpac, fgPos )
 {
+    for(let i = 0; i < fgImg.width; i++){
+        for(let j = 0; j < fgImg.height; j++){
+            let x = fgPos["x"] + i, y = fgPos["y"] + j;
+            if(x < 0 || x >= bgImg.width || y < 0 || y >= bgImg.height) continue;
+
+            let alphaf = fgOpac * fgImg.data[(j*fgImg.width+i)*4 + 3]/255.0, alphab = bgImg.data[(y*bgImg.width+x)*4 + 3]/255.0
+            let alpha = (alphaf) + (1-alphaf) * alphab;
+            bgImg.data[(y*bgImg.width+x)*4 + 3] = alpha * 255
+            bgImg.data[(y*bgImg.width+x)*4] = (fgImg.data[(j*fgImg.width+i)*4] * alphaf + (1 - alphaf) * alphab * bgImg.data[(y*bgImg.width+x)*4])/alpha
+            bgImg.data[(y*bgImg.width+x)*4 + 1] = (fgImg.data[(j*fgImg.width+i)*4+1] * alphaf + (1 - alphaf) * alphab * bgImg.data[(y*bgImg.width+x)*4+1])/alpha
+            bgImg.data[(y*bgImg.width+x)*4 + 2] = (fgImg.data[(j*fgImg.width+i)*4+2] * alphaf + (1 - alphaf) * alphab * bgImg.data[(y*bgImg.width+x)*4+2])/alpha
+        }
+    }
+
 }
